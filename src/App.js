ada42691acf04168a81642e82ad67709c757f9a3
Cara Steffen <cara@treadstonedesign.com>
2019-11-16 15:07:26 -0700

    Zeit initial deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <h1>Website coming soon</h1>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  API_KEY: "insert_your_api_key"
+};
