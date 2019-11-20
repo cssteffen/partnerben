fbdd898f078e483b638b542234dfe7c9c2a7fdf3
Cara Steffen <cara@treadstonedesign.com>
2019-11-20 06:59:54 -0700

    fixed landing page button links

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./buttons.css";
 
 export default class Buttons extends Component {
   render() {
     return (
       <div>
-        <button>Register</button>
-        <button>Demo</button>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+        <Link to="/dashboard">
+          <button>Demo</button>
+        </Link>
       </div>
     );
   }
